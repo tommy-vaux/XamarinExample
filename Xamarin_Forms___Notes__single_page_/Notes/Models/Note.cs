7ec5a7aadaa8c857f9ea035a4a240b0a43111cd3
tommy-vaux <tommyvaux@icloud.com>
2020-03-23 05:19:00 +1100

    Added Notes Example (simple saving & navigation example)
    Note: I DID NOT WRITE THIS, credit goes to the microsoft note-taking app xamarin example. This project generally is meant to just be a dumping ground for xamarin test/practice projects.

---
+++ Xamarin_Forms___Notes__single_page_/Notes/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes.Models
+{
+        // An example of how to implement a simple data class for storing data for each note.
+        class Note
+        {
+                public string Filename { get; set; }
+                public string Text { get; set; }
+
+                public DateTime Date { get; set; }
+        }
+}
